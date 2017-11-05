928a4e663557ff7e67a23cc12daa08eead02e42f
Arshad Shaikh <Arshad Shaikh@ArshadShaikh>
2017-11-05 20:21:24 +0530

    new Commit test

--- GitTest/src/org/test/TestClass.java
+++ GitTest/src/org/test/TestClass.java
@@ -4,8 +4,7 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		System.out.println("Test Git");
-		System.out.println("TEsting comit");
-		System.out.println("TEsting merge");
+		System.out.println("Testing new commit");
 	}
 
 }
